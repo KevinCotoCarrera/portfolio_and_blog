9f3c319210bcb50f72119ff47e9ad05a04302dd0
Kevin <kevincoto2002@gmail.com>
2021-12-01 17:35:12 -0500

    first commit

---
+++ components/CardProject.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Image from 'next/image';
+import {
+  Box,
+  Center,
+  Heading,
+  Text,
+  Stack,
+  useColorModeValue,
+  Link
+} from '@chakra-ui/react';
+import { animate } from 'framer-motion';
+import link from 'next/link';
+
+export default function CardProject({img, name, type, text, link}) {
+  return (
+    <Center py={6}>
+      <Box
+        w={['350px', '500px']}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}
+        _hover={{
+            rotate: '10',
+            transition: '1s'
+        }}>
+        <Box
+          h={'150px','250px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}>
+          <Image
+            src={img}
+            layout={'fill'}
+          />
+        </Box>
+        <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}>
+            {type}
+          </Text>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}>
+            {name}
+          </Heading>
+          <Text color={'gray.500'}
+          >
+            {text}
+          </Text>
+        </Stack>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+         <Link href={link} className="text-gray-800 dark:text-gray-800">{link}</Link>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
